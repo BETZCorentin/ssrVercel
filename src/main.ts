0b795e4af16909da7d69b1f09b9d734b2710f6e1
CorentinBETZ <corentin@captnboat.com>
2024-02-13 11:16:25 +0100

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: process.env.FRONT_URL,
     credentials: true,
   });
   app.use(cookieParser());
